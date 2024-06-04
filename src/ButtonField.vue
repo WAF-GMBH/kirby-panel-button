8aedc31d724f53108a27ba109e2c0c04ae235052
Malte Müller <malte@waf.gmbh>
2024-06-04 13:11:59 +0200

    Compatibility with Kirby 4+

--- src/ButtonField.vue
+++ src/ButtonField.vue
@@ -3,18 +3,18 @@
 
         <div class="k-button-field-button-wrapper">
 
-            <button v-if="!isLoading" type="button" class="k-button-field-button k-button k-box" :data-theme="theme" @click="onClick">
-                <k-icon :type="icon" class="k-button-icon" />
+            <button v-if="!isLoading" type="button" data-size="sm" data-has-icon="true" data-has-text="true" class="k-button-field-button k-button k-box" :data-theme="theme ? theme : 'neutral'" data-variant="filled" @click="onClick">
+                <span class="k-button-icon"><k-icon :type="icon"/></span>
                 <span class="k-button-text">{{ text }}</span>
             </button>
             
-            <div v-if="isLoading && !hasError" type="button" class="k-button-field-button k-button k-button-disabled k-box" data-disabled="true">
-                <k-icon type="dots" class="k-button-icon" />
+            <div v-if="isLoading && !hasError" type="button" data-size="sm" data-has-icon="true" data-has-text="true" class="k-button k-button-disabled k-box" data-variant="filled" data-disabled="true">
+                <span class="k-button-icon"><k-icon :type="dots"/></span>
                 <span class="k-button-text">Please wait</span>
             </div>
             
-            <div v-if="hasError" type="button" class="k-button-field-button k-button k-button-disabled k-box" data-disabled="true" data-theme="negative">
-                <k-icon type="alert" class="k-button-icon" />
+            <div v-if="hasError" type="button" data-size="sm" data-has-icon="true" data-has-text="true" class="k-button k-button-disabled k-box" data-disabled="true" data-variant="filled" data-theme="negative">
+                <span class="k-button-icon"><k-icon :type="alert"/></span>
                 <span class="k-button-text">Error</span>
             </div>
 
@@ -43,14 +43,8 @@
         methods: {
             async onClick(){
                 if( this.open === true ){
-                    /*
-                    open url in new tab
-                    */
                     openUrlInNewTab( this );
                 } else {
-                    /*
-                    trigger webhook
-                    */
                     triggerWebhook( this );
                 }
             }
@@ -65,9 +59,9 @@
     }
 
     .k-button-field-button {
-        display: block;
+        display: flex;
+        justify-content: flex-start;
         width: 100%;
-        text-align: left;
     }
 
 </style>
